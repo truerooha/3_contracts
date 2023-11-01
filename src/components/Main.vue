f2c650c3111b785c66622edf24bae0841139527d
rooha <github@panadey.ru>
2023-11-01 14:46:12 +0300

    отдельный компонент трей

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -17,17 +17,17 @@
       <img src="@/assets/icons/comunication.svg" alt="task">
       <h4>Коммуникаций</h4>
     </div>
-    <div class="big-card">
-      <div class="tray-area">
-        <span>Трей</span>
-      </div>
-    </div>
+    <Tray/>
   </div>
 </template>
 
 <script>
+import Tray from './Tray.vue';
+
 export default {
-    
+   components: {
+    Tray
+  } 
 }
 </script>
 
@@ -85,40 +85,4 @@ export default {
   border-radius: 20px;
   color: white;
 }
- 
-.big-card {
-  display: flex;
-  flex-direction: column;
-  color: white;
-  cursor: pointer;
-  border-radius: 16px;
-  grid-column: span 3;
-  background-color: #a0a0a0;
-  padding: 8px;
-  text-align: center;
-  margin-top: 10px;
-}
-
-.big-card:hover {
-  opacity: 90%;
-}
-
-.big-card h4 {
-  justify-self: start;
-  align-self: start;
-}
-
-.tray-area {
-  background: white;
-  border-radius: 10px;
-  width: 100%;
-  height: 100%;
-  color: #000;
-  text-align: left;
-}
-
-.tray-area span {
-  padding: 10px;
-}
-
 </style>
---
+++ src/components/Tray.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="big-card">
+      <div class="tray-area">
+        <span>Трей</span>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.big-card {
+  display: flex;
+  flex-direction: column;
+  color: white;
+  cursor: pointer;
+  border-radius: 16px;
+  grid-column: span 3;
+  background-color: #a0a0a0;
+  padding: 8px;
+  text-align: center;
+  margin-top: 10px;
+}
+
+.big-card:hover {
+  opacity: 90%;
+}
+
+.big-card h4 {
+  justify-self: start;
+  align-self: start;
+}
+
+.tray-area {
+  background: white;
+  border-radius: 10px;
+  width: 100%;
+  height: 100%;
+  color: #000;
+  text-align: left;
+}
+
+.tray-area span {
+  padding: 10px;
+}
+
+</style>
