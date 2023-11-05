1faed6231ca30d7d34429d26742b17bc68b29d1c
rooha <github@panadey.ru>
2023-11-05 18:49:47 +0300

    cp page

--- src/components/CP.vue
+++ src/components/CP.vue
@@ -1,8 +1,14 @@
 <template>
   <h1 class="page-h1">Контрагенты</h1>
-  <p>здесь возможно фильтры</p>
-  <br>
-  <p>здесь красивый список контрагентов</p>
+  <div class="page-content theme-color" id="cp-page">
+    <div id="cp-table">
+      Список
+    </div>
+
+    <div id="cp-details">
+      Детали
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,5 +18,12 @@ export default {
 </script>
 
 <style>
-
+#cp-page {
+  display: flex;
+  height: 90%;
+  border-radius: 10px;
+  padding: 30px;
+  justify-self: center;
+  align-self: center;
+}
 </style>
