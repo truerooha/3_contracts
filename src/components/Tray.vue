738fac4d3d390a89a3ab9c1a8a9a3b0352390294
rooha <github@panadey.ru>
2023-12-26 17:09:26 +0300

    перетаскивание файлов с компа

--- src/components/Tray.vue
+++ src/components/Tray.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="big-card">
-      <div class="main-area" >
+      <div class="main-area" @drop.prevent="dropHandler" @dragover.prevent="dragOverHandler" @dragleave="dragLeaveHandler">
         <div @click="toggleDropdown" class="add-el">
             <svg class="add" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12Z" stroke="currentColor" stroke-width="1.5"/>
@@ -96,6 +96,21 @@ export default {
       const fileInput = document.getElementById('fileInput')
       fileInput.click()
     },
+    dragOverHandler(event) {
+      event.target.style.border = '2px dashed #31C48D';
+    },
+     dragLeaveHandler(event) {
+      event.target.style.border = 'none';
+    },
+    dropHandler(event) {
+      var file = event.dataTransfer.files[0];
+      const fileName = file.name;
+      console.log(fileName)
+      const fileExtension = fileName.slice((fileName.lastIndexOf(".") - 1 >>> 0) + 2);
+      this.files.push({ fileType: fileExtension, fileDescrip: fileName})
+      event.target.style.border = 'none';
+
+    },
     fileSelected() {
       const selectedFiles = fileInput.files
 
@@ -186,6 +201,8 @@ p {
   grid-template-rows: repeat(auto-fill, minmax(128px, 1fr)); 
   padding: 20px;
   width: 100%;
+  border-radius: 12px;
+
 }
 
 .drop-area {
