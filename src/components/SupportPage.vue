2b49d4654d918d38308268059654d3ba390aff50
rooha <github@panadey.ru>
2024-01-29 18:41:08 +0300

    немножко поправил структуру

--- src/components/SupportPage.vue
+++ src/components/SupportPage.vue
@@ -1,14 +1,22 @@
 <template>
   <h1 class="page-h1">Помощь</h1>
-  <p>Все виды получения саппорта</p>
   <br>
-  <p>Написать тикет, открыть чат, поставить оценку</p>
+  <div class="wrapper" name="header-input">
+    <label for="header-input">Тема обращения:</label>
+    <SearchInput/>
+  </div>
 </template>
 
-<script>
-
+<script setup>
+import SearchInput from "./SearchInput.vue";
 </script>
 
 <style>
-
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  width: 60%;
+}
 </style>
