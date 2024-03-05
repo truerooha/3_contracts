1f0b149c3b2a5f2d590b344baeecc6d32bf21e23
rooha <github@panadey.ru>
2024-03-05 15:40:25 +0300

    удалили ненужное из трея

--- src/components/Tray.vue
+++ src/components/Tray.vue
@@ -26,21 +26,13 @@
           :altText="file.fileDescrip"
         />
       </div>
-      <div class="drop-area" id="drop-area" @dragover="onDragOver" @dragleave="onDragLeave" @drop="onDrop">
-        <div id="drop-text" v-if="isDropAreaVisible">
-          <h4> Новый договор</h4>
-          <p>Перенесите документы или черновик в данную область, чтобы создать договор </p>
-        </div>
-        <div v-if="!isDropAreaVisible">
-          <img class="add" src="@/assets/icons/new.svg" alt="new" @dragstart="onDragStart">
-        </div>
-      </div>
     </div>
 </template>
 
 <script>
 import TrayElement from './TrayElement.vue';
 import { getImageSrc } from '@/files'
+
 export default {
   components: { TrayElement },
   data() {
@@ -55,26 +47,6 @@ export default {
   },
   methods: {
     getImageSrc,
-    onDragStart(e) {
-      e.dataTransfer.setData('text/plain', e.target.id);
-    },
-    onDragOver(e) {
-      e.preventDefault();
-      let dropArea = document.getElementById("drop-area");
-      this.isDropAreaVisible = false;
-    },
-     onDragLeave(e) {
-      e.preventDefault();
-      this.isDropAreaVisible = true;
-    },
-    onDrop(e) {
-      e.preventDefault();
-      const draggedElementId = e.dataTransfer.getData('text/plain');
-      const draggedElement = document.getElementById(draggedElementId);
-      if (e.target.id === 'drop-area') {
-        draggedElement.style.display = 'none';
-      }
-    },
     toggleDropdown() {
       this.isDropdownVisible = !this.isDropdownVisible 
     },
@@ -88,7 +60,7 @@ export default {
       }
       event.target.style.border = '2px dashed #31C48D';
     },
-     dragLeaveHandler(event) {
+    dragLeaveHandler(event) {
       event.target.style.border = 'none';
     },
     dropHandler(event) {
@@ -199,33 +171,6 @@ p {
   width: 100%;
   border-radius: 12px;
 
-}
-
-.drop-area {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  width: 240px;
-  margin: 0;
-  padding: 10px;
-  background: #F3FAF7;
-  border: 1px dashed #31C48D;
-  border-radius: 12px;
-}
-
-.drop-area h4 {
-  font-size: 18px;
-  color: #31C48D;
-}
-
-.drop-area p {
-  font-size: 14px;
-  color: #718096;
-}
-
-.drop-area:hover {
-  background: #DEF7EC;
-  border: 1px solid #31C48D;
 }
 
 .dropdown-content {
